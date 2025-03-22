096dd023fd6ee41c276f8968874833d290e69d98
Marcin Szafranek <marcin.szafranek@gmail.com>
2025-03-22 17:48:12 +0100

    Add function with recursion

--- tests/removeLetterFromText.spec.ts
+++ tests/removeLetterFromText.spec.ts
@@ -1,19 +1,68 @@
 import {test, expect} from '@playwright/test';
 
-const text = "Lorcaem ipdsum dolbor sit amet."
+const text = "Lorcaem Ipdsumb dolbor sit ametb."
 const occurrencesToRemove = 2;
 
 const testCases = [
-    {letter: 'a', expected: 'Lorcem ipdsum dolbor sit met.'},
-    {letter: 'b', expected: 'Lorcaem ipdsum dolor sit amet.'},
-    {letter: 'c', expected: 'Loraem ipdsum dolbor sit amet.'}
+    {sentence: text, letter: 'a', expected: 'Lorcem Ipdsumb dolbor sit metb.'},
+    {sentence: text, letter: 'b', expected: 'Lorcaem Ipdsum dolor sit ametb.'},
+    {sentence: text, letter: 'c', expected: 'Loraem Ipdsumb dolbor sit ametb.'},
+    {sentence: null, letter: 'z', expected: null},
+    {sentence: "", letter: 'g', expected: ""}
 ];
 
-testCases.forEach(({letter, expected}) => {
-    test(`Remove letter ${letter} ${occurrencesToRemove} times from ${text}`, async ({}) => {
-        expect(removeLetter(text, letter, occurrencesToRemove)).toEqual(expected);
+testCases.forEach(({sentence, letter, expected}) => {
+    test(`Remove letter ${letter} ${occurrencesToRemove} times from ${sentence}`, async ({}) => {
+        expect(removeLetter(sentence, letter, occurrencesToRemove)).toEqual(expected);
     });
 });
+
+testCases.forEach(({sentence, letter, expected}) => {
+    test(`Remove letter ${letter} ${occurrencesToRemove} times from ${sentence}, use basic method`, async ({}) => {
+        expect(removeLetterBasic(sentence, letter, occurrencesToRemove)).toEqual(expected);
+    });
+});
+
+testCases.forEach(({sentence, letter, expected}) => {
+    test(`Remove letter ${letter} ${occurrencesToRemove} times from ${sentence}, use  O(n)  method`, async ({}) => {
+        expect(removeLetterEfficient(sentence, letter, occurrencesToRemove)).toEqual(expected);
+    });
+});
+
+function removeLetterBasic(sentence: string, letter: string, count: number) {
+    if (sentence === null) return null;
+
+    if (count <= 0 || !sentence.includes(letter)) return sentence;
+    const sentenceArray: string[] = sentence.split('');
+    if (sentenceArray.some(l => l === letter)) {
+        for (let i = 0; i < sentenceArray.length; i++) {
+            if (sentenceArray[i] === letter) {
+                sentenceArray.splice(i, 1);
+                break;
+            }
+        }
+    }
+    return removeLetterBasic(sentenceArray.join(''), letter, count - 1);
+}
+
+function removeLetterEfficient(sentence: string, letter: string, count: number): string {
+    if (sentence === null) return null;
+
+    let result = '';
+    let removed = 0;
+
+    for (let i = 0; i < sentence.length; i++) {
+        const char = sentence[i];
+        if (char === letter && removed < count) {
+            removed++;
+            continue;
+        }
+        result += char;
+    }
+
+    return result;
+}
+
 
 /**
  * Removes a specified number of occurrences of a given letter from a sentence, case-insensitive.
@@ -23,6 +72,8 @@ testCases.forEach(({letter, expected}) => {
  * @returns The sentence with the specified number of letter occurrences removed
  */
 function removeLetter(sentence: string, letter: string, count: number): string {
+    if (sentence === null) return null;
+
     let removed = 0;
     const regex = new RegExp(letter, 'gi');
     return sentence.replace(regex, match => removed++ < count ? '' : match);

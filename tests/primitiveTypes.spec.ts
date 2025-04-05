f6156986aba96a0e611fa0b9806893fbf5965342
Marcin Szafranek <marcin.szafranek@gmail.com>
2025-04-05 22:07:16 +0200

    Add infinity or and tests

--- tests/primitiveTypes.spec.ts
+++ tests/primitiveTypes.spec.ts
@@ -20,6 +20,13 @@ test.describe(`Primitive types`, () => {
         const result3 = 0 / 0;
         expect(result3, "Second division 0/0 should also be NaN").toEqual(NaN);
         expect(result1 == result3, "NaN is not equal to anything, even itself.").toBeFalsy();
+
+        const infinity = 1 / 0;
+        expect(typeof infinity === "number", "Infinity is of type 'number'").toBeTruthy();
+        expect(infinity == infinity - 1, "Subtracting from Infinity still results in Infinity").toBeTruthy();
+        expect(Number.isNaN(infinity * 0), "Multiplying Infinity by 0 results in NaN").toBeTruthy();
+        expect(Number.isNaN(infinity - Infinity), "Infinity minus Infinity is NaN").toBeTruthy();
+        expect(1 / Infinity === 0, "1 divided by Infinity equals 0").toBeTruthy();
     })
 
     test('falsy value', {}, async () => {
@@ -29,6 +36,32 @@ test.describe(`Primitive types`, () => {
         expect(null, "Null should be falsy in JavaScript").toBeFalsy();
         expect(undefined, "Undefined should be falsy in JavaScript").toBeFalsy();
         expect(NaN, "NaN should be falsy in JavaScript").toBeFalsy();
+    })
+
+    test('or ||', {
+        annotation: {
+            type: "primitiveTypes",
+            description: `Returns the first true value, or the last one if all are false.`
+        },
+    }, async () => {
+        expect(0 || -0).toBe(-0);
+        expect(-0 || 0).toBe(0);
+        expect(0 || 'test').toBe('test');
+        // @ts-ignore
+        expect('test' || 0).toBe('test');
+    })
+
+    test('and &&', {
+        annotation: {
+            type: "primitiveTypes",
+            description: `Returns the first falsy value, or the last one if all are truthy`
+        },
+    }, async () => {
+        expect(-0 && 0).toBe(-0);
+        // @ts-ignore
+        expect('aaa' && null).toBe(null);
+        // @ts-ignore
+        expect('aaa' && 'bbb').toBe('bbb');
     })
 });
 
